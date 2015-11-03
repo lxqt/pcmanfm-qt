8106aa787fede3df59bce43a01a56da7a33bc268
Tsu Jan <tsujan2000@gmail.com>
2015-11-03 17:39:40 +0330

    Tab context menu for closing other tabs
    This is a right-click menu, only for tabs and only when there is more than one tab, for closing the left, right or other tabs, depending on the the right clicked tab, which can be the current one or not. It comes in handy when there are several tabs.

--- pcmanfm/mainwindow.h
+++ pcmanfm/mainwindow.h
@@ -143,6 +143,14 @@ protected Q_SLOTS:
 
   void onBackForwardContextMenu(QPoint pos);
 
+  void tabContextMenu(const QPoint& pos);
+  void closeLeftTabs();
+  void closeRightTabs();
+  void closeOtherTabs() {
+    closeLeftTabs();
+    closeRightTabs();
+  }
+
 protected:
   // void changeEvent( QEvent * event);
   void closeTab(int index);
@@ -162,6 +170,7 @@ private:
   QLabel* fsInfoLabel;
   FmBookmarks* bookmarks;
   Launcher fileLauncher_;
+  int rightClickIndex;
 };
 
 }
