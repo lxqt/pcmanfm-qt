8635d7631344ee0f68d7a61a5788b12bc2cb6d92
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-01-15 22:45:33 +0800

    Add some ``beautiful'' workarounds that bypass Qt bug #49947 and 47981. This should fix the bug lxqt/#688 temporarily.

--- pcmanfm/pcmanfm.cpp
+++ pcmanfm/pcmanfm.cpp
@@ -1,6 +1,7 @@
 #include <libfm/fm.h>
 #include "application.h"
 #include <libfm-qt/libfmqt.h>
+#include "xdndworkaround.h"
 
 int main(int argc, char** argv) {
   // ensure that glib integration of Qt is not turned off
@@ -8,6 +9,8 @@ int main(int argc, char** argv) {
   qunsetenv("QT_NO_GLIB");
 
   PCManFM::Application app(argc, argv);
+  // The XDND support of Qt5 is broken, let's install our own workarounds
+  WORKAROUND_QT5_XDND_BUG(workaround);
   app.init();
   return app.exec();
 }
