c6cf50af303994e030fd27515dca7dd09d5dd871
npmiller <nicolas.pierre.miller@gmail.com>
2016-02-03 21:42:14 +0000

    Add quit shortcut

--- pcmanfm/mainwindow.cpp
+++ pcmanfm/mainwindow.cpp
@@ -166,6 +166,9 @@ MainWindow::MainWindow(FmPath* path):
 
   // create shortcuts
   QShortcut* shortcut;
+  shortcut = new QShortcut(QKeySequence::Quit, this);
+  connect(shortcut, &QShortcut::activated, this, &MainWindow::close);
+
   shortcut = new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_L), this);
   connect(shortcut, &QShortcut::activated, this, &MainWindow::focusPathEntry);
 
