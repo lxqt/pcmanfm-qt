30abc55da3fef4e107a3029bd605d211de65c2a1
Alf Gaida <agaida@siduction.org>
2017-06-27 18:26:33 +0200

    set Qt::AA_UseHighDpiPixmaps to true

--- pcmanfm/pcmanfm.cpp
+++ pcmanfm/pcmanfm.cpp
@@ -3,11 +3,13 @@
 #include <libfm-qt/libfmqt.h>
 
 int main(int argc, char** argv) {
-  // ensure that glib integration of Qt is not turned off
-  // This fixes #168: https://github.com/lxde/pcmanfm-qt/issues/168
-  qunsetenv("QT_NO_GLIB");
+    // ensure that glib integration of Qt is not turned off
+    // This fixes #168: https://github.com/lxde/pcmanfm-qt/issues/168
+    qunsetenv("QT_NO_GLIB");
 
-  PCManFM::Application app(argc, argv);
-  app.init();
-  return app.exec();
+    PCManFM::Application app(argc, argv);
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+
+    app.init();
+    return app.exec();
 }
