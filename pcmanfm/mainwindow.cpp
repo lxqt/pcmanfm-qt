ed87cd6d8e1ea6eea0a54130c28d6012454a7db7
Tsu Jan <tsujan2000@gmail.com>
2017-07-20 17:27:41 +0430

    Inline renaming with F2
    This depends on and follows https://github.com/lxde/libfm-qt/pull/98. It implements inline renaming with `F2` for single files.

--- pcmanfm/mainwindow.cpp
+++ pcmanfm/mainwindow.cpp
@@ -1044,10 +1044,21 @@ void MainWindow::on_actionDelete_triggered() {
 }
 
 void MainWindow::on_actionRename_triggered() {
-    TabPage* page = currentPage();
-    auto files = page->selectedFiles();
-    for(auto& file: files) {
-        Fm::renameFile(file, nullptr);
+    // do inline renaming if only one item is selected,
+    // otherwise use the renaming dialog
+     TabPage* page = currentPage();
+     auto files = page->selectedFiles();
+    if(files.size() == 1) {
+        QModelIndex cur = page->folderView()->childView()->currentIndex();
+        if (cur.isValid()) {
+            page->folderView()->childView()->scrollTo(cur);
+            page->folderView()->childView()->edit(cur);
+        }
+    }
+    else if(!files.empty()) {
+        for(auto& file: files) {
+            Fm::renameFile(file, nullptr);
+        }
     }
 }
 
