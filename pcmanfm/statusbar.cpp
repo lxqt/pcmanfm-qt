3191950cb83167bc48a21716a21e95964c12c802
Tsu Jan <tsujan2000@gmail.com>
2017-09-28 10:11:06 +0330

    Text eliding, long texts and newline
    Middle eliding is chosen to show more important info as far as possible.

--- pcmanfm/statusbar.cpp
+++ pcmanfm/statusbar.cpp
@@ -15,16 +15,49 @@
 */
 
 #include "statusbar.h"
+#include <QPainter>
+#include <QStyleOption>
 
 #define MESSAGE_DELAY 250
 
 namespace PCManFM {
 
+Label::Label(QWidget* parent, Qt::WindowFlags f):
+  QLabel(parent, f),
+  lastWidth_(0) {
+    setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    // set a min width to prevent the window from widening with long texts
+    setMinimumWidth(fontMetrics().averageCharWidth() * 10);
+}
+
+// A simplified version of QLabel::paintEvent()
+// without pixmap or shortcut but with eliding.
+void Label::paintEvent(QPaintEvent* /*event*/) {
+    QRect cr = contentsRect().adjusted(margin(), margin(), -margin(), -margin());
+    QString txt = text();
+    // if the text is changed or its rect is resized (due to window resizing),
+    // find whether it needs to be elided...
+    if (txt != lastText_ || cr.width() != lastWidth_) {
+        lastText_ = txt;
+        lastWidth_ = cr.width();
+        elidedText_ = fontMetrics().elidedText(txt, Qt::ElideMiddle, cr.width());
+    }
+    // ... then, draw the (elided) text
+    if(!elidedText_.isEmpty()) {
+        QPainter painter(this);
+        QStyleOption opt;
+        opt.initFrom(this);
+        style()->drawItemText(&painter, cr, alignment(), opt.palette, isEnabled(), elidedText_, foregroundRole());
+    }
+}
+
 StatusBar::StatusBar(QWidget *parent):
   QStatusBar(parent),
   lastTimeOut_(0) {
-    statusLabel_ = new QLabel();
-    statusLabel_->setIndent(4);
+    statusLabel_ = new Label();
+    statusLabel_->setFrameShape(QFrame::NoFrame);
+    // 4px space on both sides (not to be mixed with the permanent widget)
+    statusLabel_->setContentsMargins(4, 0, 4, 0);
     addWidget(statusLabel_);
 
     messageTimer_ = new QTimer (this);
@@ -51,8 +84,9 @@ void StatusBar::showMessage(const QString &message, int timeout) {
 
 void StatusBar::reallyShowMessage() {
     if(lastTimeOut_ == 0) {
-        // set the text on the label for it not to change when a menubar item is focused
-        statusLabel_->setText(lastMessage_);
+        // set the text on the label to prevent its disappearance on focusing menubar items
+        // and also ensure that it contsains no newline (because file names may contain it)
+        statusLabel_->setText(lastMessage_.replace(QLatin1Char('\n'), QLatin1Char(' ')));
     }
     else {
         QStatusBar::showMessage(lastMessage_, lastTimeOut_);
