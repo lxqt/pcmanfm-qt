276ad0cc90b57bb4894f7c88245bb7ccf09ff6a1
p-bo <pavel.borecki@gmail.com>
2018-10-24 15:52:04 +0000

    Translated using Weblate (Czech)
    Currently translated at 100,0% (335 of 335 strings)
    
    Translation: LXQt/PCManFM-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/pcmanfm-qt/cs/

--- pcmanfm/translations/pcmanfm-qt_cs.ts
+++ pcmanfm/translations/pcmanfm-qt_cs.ts
@@ -442,7 +442,7 @@ Je také vyhrazeno místo pro 3 řádky textu.</translation>
     <message>
         <location filename="../main-win.ui" line="142"/>
         <source>&amp;Filtering</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Filtrování</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="273"/>
@@ -627,32 +627,32 @@ Je také vyhrazeno místo pro 3 řádky textu.</translation>
     <message>
         <location filename="../main-win.ui" line="820"/>
         <source>&amp;Show/Focus Filter Bar</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zobrazit/skrýt lištu filtrování</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="823"/>
         <source>Show Filter Bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazit lištu filtrování</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="826"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+I</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="834"/>
         <source>S&amp;plit View</source>
-        <translation type="unfinished"></translation>
+        <translation>Rozdělený &amp;pohled</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="837"/>
         <source>Split View</source>
-        <translation type="unfinished"></translation>
+        <translation>Rozdělený pohled</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="840"/>
         <source>F6</source>
-        <translation type="unfinished">F6</translation>
+        <translation>F6</translation>
     </message>
     <message>
         <source>&amp;Filter</source>
@@ -666,12 +666,12 @@ Je také vyhrazeno místo pro 3 řádky textu.</translation>
     <message>
         <location filename="../main-win.ui" line="723"/>
         <source>&amp;Clear All Filters</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Vyčistit všechny filtry</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="726"/>
         <source>Ctrl+Shift+K</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Shift+K</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="735"/>
@@ -726,7 +726,7 @@ Je také vyhrazeno místo pro 3 řádky textu.</translation>
     <message>
         <location filename="../main-win.ui" line="715"/>
         <source>Permanent &amp;filter bar</source>
-        <translation type="unfinished"></translation>
+        <translation>Trvalá lišta &amp;filtrování</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="757"/>
@@ -1594,7 +1594,7 @@ pouze pokud je více než jedna karta.</translation>
     <message>
         <location filename="../preferences.ui" line="796"/>
         <source>Application restart is needed for changes to take effect.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aby se změny projevily, je třeba aplikaci restartovat.</translation>
     </message>
     <message>
         <source>If unchecked, the tab bar will be positioned only
@@ -1785,7 +1785,7 @@ pouze nad složkový pohled a ne nad levé podokno.</translation>
     <message>
         <location filename="../tabpage.cpp" line="62"/>
         <source>Clear text (Ctrl+K)</source>
-        <translation type="unfinished">Vyprázdnit text (Ctrl+K)</translation>
+        <translation>Vymazat text (Ctrl+K)</translation>
     </message>
 </context>
 <context>
@@ -1793,7 +1793,7 @@ pouze nad složkový pohled a ne nad levé podokno.</translation>
     <message>
         <location filename="../tabpage.cpp" line="92"/>
         <source>Filter:</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtr:</translation>
     </message>
 </context>
 </TS>
