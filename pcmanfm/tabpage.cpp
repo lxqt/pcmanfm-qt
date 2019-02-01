ae0f337db06faf0e46cafd195dba8963da8a062d
Tsu Jan <tsujan2000@gmail.com>
2019-02-01 09:53:26 +0330

    Fixed status message on changing view from/to detailed list
    Fixes https://github.com/lxqt/pcmanfm-qt/issues/890
    
    The view is recreated when going to/from the detailed list; so the status message should be updated.

--- pcmanfm/tabpage.cpp
+++ pcmanfm/tabpage.cpp
@@ -739,11 +739,14 @@ void TabPage::setViewMode(Fm::FolderView::ViewMode mode) {
     Fm::FolderView::ViewMode prevMode = folderView_->viewMode();
     folderView_->setViewMode(mode);
     folderView_->childView()->setFocus();
-    if(!settings.showFilter() && prevMode != folderView_->viewMode()) {
+    if(prevMode != folderView_->viewMode()) {
         // FolderView::setViewMode() may delete the view to switch between list and tree.
-        // So, the event filter should be re-installed.
-        folderView_->childView()->removeEventFilter(this);
-        folderView_->childView()->installEventFilter(this);
+        // So, the event filter should be re-installed and the status message should be updated.
+        if(!settings.showFilter()) {
+            folderView_->childView()->removeEventFilter(this);
+            folderView_->childView()->installEventFilter(this);
+        }
+        onSelChanged();
     }
 }
 
