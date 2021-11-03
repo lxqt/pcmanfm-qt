f37dfb8c5897f0ad076503967c731964969204a9
Weblate <noreply@weblate.org>
2021-11-03 10:50:44 +0100

    Weblate commits
    Co-authored-by: Pavel Shlyak <shlyak-pavel2011@ya.ru>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/es/
    Translation: LXQt Desktop/PCManFm-qt

--- pcmanfm/translations/pcmanfm-qt_es.ts
+++ pcmanfm/translations/pcmanfm-qt_es.ts
@@ -539,7 +539,7 @@ es el gestor de archivos predeterminado.</translation>
     <message>
         <location filename="../shortcuts.ui" line="31"/>
         <source>Esc</source>
-        <translation>Esc</translation>
+        <translation type="unfinished">Esc</translation>
     </message>
     <message>
         <location filename="../shortcuts.ui" line="34"/>
@@ -1122,7 +1122,7 @@ es el gestor de archivos predeterminado.</translation>
     <message>
         <location filename="../main-win.ui" line="927"/>
         <source>Hidden &amp;Shortcuts</source>
-        <translation type="unfinished">Atajo&amp;s de teclado ocultos</translation>
+        <translation>Atajos ocultos</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="932"/>
@@ -2082,7 +2082,7 @@ miniaturas grandes, afecta tanto a las imagenes como a las carpetas</translation
     <message>
         <location filename="../preferences.ui" line="429"/>
         <source>3 px by default.</source>
-        <translation>De 3&#xa0;px de manera predeterminada.</translation>
+        <translation>De 3 px de manera predeterminada.</translation>
     </message>
     <message>
         <location filename="../preferences.ui" line="432"/>
@@ -2099,7 +2099,7 @@ miniaturas grandes, afecta tanto a las imagenes como a las carpetas</translation
         <location filename="../preferences.ui" line="452"/>
         <source>3 px by default.
 A space is also reserved for 3 lines of text.</source>
-        <translation>De 3&#xa0;px de manera predeterminada.
+        <translation>De 3 px de manera predeterminada.
 También se reserva espacio para 3 renglones de texto.</translation>
     </message>
     <message>
@@ -2132,7 +2132,7 @@ visible cuando haya más de una pestaña abierta.</translation>
 Usually, most image types are supported. The default size limit is 4 MiB.</source>
         <translation>El miniaturizador integrado crea miniaturas de las imágenes que Qt admite.
 
-Por lo general, la mayoría de los tipos de imagen es compatible. El límite de tamaño predeterminado es de 4&#xa0;MiB.</translation>
+Por lo general, la mayoría de los tipos de imagen es compatible. El límite de tamaño predeterminado es de 4 MiB.</translation>
     </message>
     <message>
         <location filename="../preferences.ui" line="633"/>
