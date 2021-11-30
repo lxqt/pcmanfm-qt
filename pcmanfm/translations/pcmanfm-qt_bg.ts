82280c7d874e311cdac2045b446397c230f7b2cf
Weblate <noreply@weblate.org>
2021-11-30 20:08:31 +0100

    Weblate commits
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/fr/
    Translation: LXQt Desktop/PCManFm-qt

--- pcmanfm/translations/pcmanfm-qt_bg.ts
+++ pcmanfm/translations/pcmanfm-qt_bg.ts
@@ -22,7 +22,8 @@
         <location filename="../about.ui" line="58"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/lxqt/pcmanfm-qt&quot;&gt;https://github.com/lxqt/pcmanfm-qt&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
 </source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://github.com/lxqt/pcmanfm-qt&quot;&gt;https://github.com/lxqt/pcmanfm-qt&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;
+</translation>
     </message>
     <message>
         <location filename="../about.ui" line="91"/>
@@ -687,7 +688,7 @@ are left clicked, even when it is not the default file manager.</source>
     <message>
         <location filename="../main-win.ui" line="81"/>
         <source>Recent F&amp;iles</source>
-        <translation type="unfinished"></translation>
+        <translation>Последни файлове</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="139"/>
@@ -957,7 +958,7 @@ are left clicked, even when it is not the default file manager.</source>
     <message>
         <location filename="../main-win.ui" line="972"/>
         <source>&amp;Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Свободно</translation>
     </message>
     <message>
         <location filename="../main-win.ui" line="642"/>
@@ -2028,7 +2029,7 @@ They might be useful if those folders are created again.</source>
     <message>
         <location filename="../preferences.ui" line="244"/>
         <source>Number of recent files:</source>
-        <translation type="unfinished"></translation>
+        <translation>Брой на последните файлове:</translation>
     </message>
     <message>
         <location filename="../preferences.ui" line="325"/>
