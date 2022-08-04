033792938dc1c91f4debf5fdbb08d14b00612710
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-08-04 13:26:32 +0200

    Weblate commits (#1623)
    Co-authored-by: Jung-Kyu Park <bagjunggyu@gmail.com>
    Co-authored-by: Pavel Shlyak <shlyak-pavel2011@ya.ru>
    Co-authored-by: kirusyaga <kirusyaga123@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/pcmanfm-qt/ru/
    Translation: LXQt Desktop/PCManFm-qt

--- pcmanfm/translations/pcmanfm-qt_ru.ts
+++ pcmanfm/translations/pcmanfm-qt_ru.ts
@@ -130,12 +130,12 @@ Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  021
     <message>
         <location filename="../bulk-rename.ui" line="80"/>
         <source>Pad numbers with zero if possible</source>
-        <translation type="unfinished"></translation>
+        <translation>По возможности окружать числа нулями</translation>
     </message>
     <message>
         <location filename="../bulk-rename.ui" line="87"/>
         <source>Use localized numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Использовать локализованные числа</translation>
     </message>
     <message>
         <location filename="../bulk-rename.ui" line="27"/>
@@ -417,7 +417,7 @@ A space is also reserved for 3 lines of text.</source>
     <message>
         <location filename="../desktop-preferences.ui" line="230"/>
         <source>Margins of work area:</source>
-        <translation type="unfinished"></translation>
+        <translation>Границы рабочей области:</translation>
     </message>
     <message>
         <location filename="../desktop-preferences.ui" line="303"/>
@@ -2226,7 +2226,7 @@ A value of -1 means that there is no limit for the file size (the default).</sou
     <message>
         <location filename="../preferences.ui" line="934"/>
         <source>Maximum search history:</source>
-        <translation type="unfinished"></translation>
+        <translation>Максимум хранимой истории поиска:</translation>
     </message>
     <message>
         <location filename="../preferences.ui" line="948"/>
