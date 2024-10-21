dcf4d341bbe03107f49e08025779ed96c7c87c26
Ian Farrell <ianfarrell47@gmail.com>
2024-10-21 07:30:24 -0400

    Prefer QStringLiteral

--- pcmanfm/mainwindow.cpp
+++ pcmanfm/mainwindow.cpp
@@ -175,9 +175,9 @@ MainWindow::MainWindow(Fm::FilePath path):
     // menu
     ui.actionDelete->setText(settings.useTrash() ? tr("&Move to Trash") : tr("&Delete"));
     ui.actionDelete->setIcon(settings.useTrash() ? QIcon::fromTheme(QStringLiteral("user-trash")) : QIcon::fromTheme(QStringLiteral("edit-delete")));
-    ui.actionNetwork->setIcon(QIcon::fromTheme(QLatin1String("network"), QIcon::fromTheme(QLatin1String("folder-network"))));
-    ui.actionApplications->setIcon(QIcon::fromTheme(QLatin1String("system-software-install"),
-        QIcon::fromTheme(QLatin1String("applications-accessories"))));
+    ui.actionNetwork->setIcon(QIcon::fromTheme(QStringLiteral("network"), QIcon::fromTheme(QStringLiteral("folder-network"))));
+    ui.actionApplications->setIcon(QIcon::fromTheme(QStringLiteral("system-software-install"),
+        QIcon::fromTheme(QStringLiteral("applications-accessories"))));
 
     // side pane
     ui.sidePane->setVisible(settings.isSidePaneVisible());
